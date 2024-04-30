f90bb75b1e3fa885924bcd8ba8261ca420cec698
Gianlu2805 <86599945+Gianlu2805@users.noreply.github.com>
2024-04-30 10:44:17 +0200

    Update JsonManager.py

--- JsonManager.py
+++ JsonManager.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class JsonManager(object):
     file_name = "dati.json"
     file = None
-    data = {"date_time": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "temperature": [], "humidity": [], "heat_index": [], "light": []}
+    data = {"device_id": "", "date_time": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "temperature": [], "humidity": [], "heat_index": [], "light": []}
 
     def __init__(self):
         return
@@ -15,6 +15,13 @@ class JsonManager(object):
     def write_data(self, data):
         self.open_file("a")
         self.file.write("\n")
+
+        for i in range(data["temperature"].lenght):
+            self.data["temperature"].extend(data["temperature"][i])
+            self.data["humidity"].extend(data["humidity"][i])
+            self.data["heat_index"].extend(data["heat_index"][i])
+            self.data["light"].extend(data["light"][i])
+
         json.dump(data, self.file)
         self.close()
         return
@@ -36,3 +43,9 @@ class JsonManager(object):
 
     def remove_data_from_file(self):
         self.open_file("w")
+
+
+    def close(self):
+        self.file.close()
+        self.file = None
+        return
