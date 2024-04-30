49c9ddbb87b868ff144e6c46c96a38f77bc7ea84
Gianlu2805 <86599945+Gianlu2805@users.noreply.github.com>
2024-04-30 09:51:45 +0200

    Add files via upload

---
+++ JsonManager.py
@@ -0,0 +1,38 @@
+# Gianluca Galanti 4C-IN
+# Json manager class
+import json
+from datetime import datetime
+
+
+class JsonManager(object):
+    file_name = "dati.json"
+    file = None
+    data = {"date_time": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "temperature": [], "humidity": [], "heat_index": [], "light": []}
+
+    def __init__(self):
+        return
+
+    def write_data(self, data):
+        self.open_file("w")
+        json.dump(data, self.file)
+        self.close()
+        return
+
+    def read_data(self):
+        self.open_file("r")
+        data = json.load(self.file)
+        return data
+
+    def open_file(self, mode):
+        if self.file is not None:
+            self.close()
+
+        try:
+            self.file = open(self.file_name, mode)
+        except:
+            print("Error while opening the file.\nClose the file before opening it")
+
+    def close(self):
+        self.file.close()
+        self.file = None
+        return
